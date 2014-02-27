d011c9a21d614c251ffc156a690a816c315af0d2
topdreg <percival@uoregon.edu>
2014-02-27 15:41:29 -0800

    Create main.css

---
+++ main.css
@@ -0,0 +1,37 @@
+
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8"/>
+    <title>Virtual Sorting Hat</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+  </head>
+
+  <body>
+    <header>
+      <p>Virtual Sorting Hat<p> 
+    </header>
+    <div class="main">
+      <div id="gryffindor" class="houseCol">
+        <img src="images/gryffindor.png">
+        <h4>Gryffindor</h4>
+      </div>
+      <div id="slytherin" class="houseCol">
+        <img src="images/slytherin.png">
+        <h4>Slytherine</h4>
+      </div>
+      <div id="ravenclaw" class="houseCol">
+        <img src="images/ravenclaw.png">
+        <h4>Ravenclaw</h4>        
+      </div>
+      <div id="hufflepuff" class="houseCol">
+        <img src="images/hufflepuff.png">
+        <h4>Hufflepuff</h4> 
+      </div>
+      <div id="muggle" class="houseCol">
+        <img src="images/muggle.png">
+        <h4>Muggle</h4>
+      </div>
+    </div>
+  </body>
+</html>
