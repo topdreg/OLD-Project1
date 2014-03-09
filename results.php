64d06aaa229308ad15040aee0463b1518b4d8db2
topdreg <percival@uoregon.edu>
2014-03-09 13:19:07 -0700

    Update results.php

--- results.php
+++ results.php
@@ -1,34 +1,77 @@
 
  <!DOCTYPE html>
 <?php
+if(isset($_POST['results'])){
+        //error_reporting(E_ALL);
+        //ini_set('display_errors', '1');
+        
+        include('appData.txt');
+        
+        try 
+        { 
+         $dbh = new PDO('mysql:host='.$server.';port='.$port.';dbname='.$dbname, $user, $pass);
+        } catch (PDOException $e) {
+         print $e->getMessage();
+         exit;
+        } 
+        $fname = $_POST["fname"];
+        $lname = $_POST["lname"];
+        $profPic = $_POST["profPic"];
         $gryff = $_POST["gryff"];
         $slyth = $_POST["slyth"];
         $rave = $_POST["rave"];
         $huff = $_POST["huff"];
         $mugg = $_POST["mugg"];
 
+
         $max = max($gryff, $slyth, $rave, $huff, $mugg);
 
         if($max == $gryff){
                 $house = "Gryffindor";
+                $housePHP = "g";
                 $img = "images/gryffindor.png";
         }
         if($max == $slyth){
                 $house = "Slytherin";
+                $housePHP = "s";
                 $img = "images/slytherin.png";
         }
         if($max == $rave){
                 $house = "Ravenclaw";
+                $housePHP = "r";
                 $img = "images/ravenclaw.png";
         }
         if($max == $huff){
                 $house = "Hufflepuff";
+                $housePHP = "h";
                 $img = "images/hufflepuff.png";
         }
         if($max == $mugg){
                 $house = "Muggle Life";
+                $housePHP = "m";
                 $img = "images/muggle.png";
         }
+        
+        $query = "INSERT INTO player VALUES(?,?,?,?,?,?,?,?,?)";
+
+        $stmt = $dbh->prepare($query);
+
+        $stmt->bindParam(1, $fname);
+        $stmt->bindParam(2, $lname);
+        $stmt->bindParam(3, $profPic);
+        $stmt->bindParam(4, $gryff);
+        $stmt->bindParam(5, $slyth);
+        $stmt->bindParam(6, $rave);
+        $stmt->bindParam(7, $huff);
+        $stmt->bindParam(8, $mugg);
+        $stmt->bindParam(9, $housePHP);
+
+        $stmt->execute();
+
+}
+else{
+        header('Location: index.php');
+}
 ?>
 <html lang="en">
 <head>
@@ -50,7 +93,5 @@
 
                         <a href="houses.php">See who else got sorted &#62;</a>
                 </div>
-                <script type="text/javascript" src="js/results.js"></script>
-                <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>  
         </body>
 </html>
