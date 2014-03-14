85d186ef01df80633e2c698b3ce8e66c5965efee
topdreg <percival@uoregon.edu>
2014-03-13 22:03:12 -0700

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
 <?php
 if(isset($_GET['errorMssg'])){
         if($_GET['errorMssg'] == "name"){
-                echo "<script> alert('Please enter both a first and last name')</script>";
+                echo "<script> alert('You must enter a first name, last name, username and password')</script>";
         }
         if($_GET['errorMssg'] == "image"){
                 echo "<script> alert('There was an error in your image, please choose another')</script>";
         }
-                if($_GET['errorMssg'] == "file"){
-                echo "<script> alert('You choose an invalid file, please choose another')</script>";
+        if($_GET['errorMssg'] == "file"){
+                echo "<script> alert('You chose an invalid file, please choose another')</script>";
+        }
+        if($_GET['errorMssg'] == "passLength"){
+                echo "<script> alert('Your password must be at least 8 characters long')</script>";
+        }
+        if($_GET['errorMssg'] == "username"){
+                echo "<script> alert('There is already a user with your chosen username, please choose another one')</script>";
         }
 }
 ?>
@@ -21,20 +27,27 @@ if(isset($_GET['errorMssg'])){
 
         <body>
                 <header>
-                        <p>Virtual Sorting Hat<p>       
+                        <a href="index.php">Virtual Sorting Hat</a>     
                 </header>
                 <div class="main">
                 <h3>Create A Profile</h3>
-                        <form id="login" action="questions.php" method="post" enctype="multipart/form-data">
+                        <div id="login"><p>Already a User? &nbsp</p><a href="login.php">Login</a></div>
+                        <form id="createProfile" action="questions.php" method="post" enctype="multipart/form-data">
                                 <label>First Name</label>
-                                <input type="text" size="50" id="fname" name="fname">
+                                <input type="text" size="35" id="fname" name="fname">
                                 <label>Last Name</label>
-                                <input type="text" size="50" id="lname" name="lname">
-                                <label>Profile Picture</label>
+                                <input type="text" size="35" id="lname" name="lname">
+                                <label>Username</label>
+                                <input type="text" size="35" id="username" name="username">
+                                <label>Password</label>
+                                <input type="password" size="35" id="password" name="password">
+                                <label>Profile Picture</label><br/>
+                                <label id="note">Images must be a gif, jpg, jpeg, or png less than 500kb</label>
                                 <input type="file" accept="image/*" id="profPic" name="profPic">
                                 <div id="imgPrevDiv"></div>
                                 <input type="submit" value="Sort Me!" id="submit" name="submit">
                         </form>
+
                 </div>
                 <script src= "js/jquery-1.11.0.min.js"></script>
                 <script>
@@ -53,7 +66,6 @@ if(isset($_GET['errorMssg'])){
                 $("#imgPrevDiv").append('<img src="#" id="imgPrev" width="100" height="auto">');
                 readURL(this);
         });
-        
                 </script>
         </body>
 </html>
